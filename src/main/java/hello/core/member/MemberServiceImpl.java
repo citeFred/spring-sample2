60d909081c1bb11c530497b1f30767ef1afe27d4
INYONG KIM <ohnyong87@gmail.com>
2024-04-02 16:41:50 +0900

    docs : 리팩토링 필요 부분

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // <- 구현체가 직접 작성되어 의존돼고 있는 문제점이 있다. (DIP 위반)
 
     @Override
     public void join(Member member) {
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService = new MemberServiceImpl(); // <- 구현체가 직접 작성되어 의존돼고 있는 문제점이 있다. (DIP 위반)
     @Test
     void join(){
         //given
