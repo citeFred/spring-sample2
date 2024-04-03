bce72c49fcaa072062390c7dd80c5b50ffc948da
INYONG KIM <ohnyong87@gmail.com>
2024-04-03 11:43:54 +0900

    refactor : 전체 구조파악하기 용이하도록 AppConfig 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,13 +10,28 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-    // 어플리케이션 동작에 필요한 구현체 생성하고 주입하는 역할만 하는 AppConfig 작성
-    // 각 서비스 계층은 추상에만 의존, 기능 구현에만 집중 하도록 할 수 있다. DIP 준수
+    // 해당 설정 클래스에서는 구현체의 역할 들을 정확하게 알수 없는 문제점이 있다.
+    // 다음처럼 수정해본다.
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+    //memberService에서는 memberRepository를 사용한다고 이해 가능
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    //memberRepository는 MemoryMemberRepository를 사용한다고 이해 가능
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+    //orderService는 memberRepository와 discountPolicy를 사용한다고 이해 가능
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+    //discountPolicy는 FixDiscountPolicy를 사용한다고 이해 가능.
+
+    //다음 처럼 전체 앱 구조를 이해하기 편해진다.
 }
