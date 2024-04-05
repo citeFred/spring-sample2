8fd66bf5017671539f22882c993ef24796480bb9
INYONG KIM <ohnyong87@gmail.com>
2024-04-05 15:08:02 +0900

    refactor : @ComponentScan과 @Autowired 사용하기

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,12 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ANNOTATION, classes = Configuration.class)
+) // 어노테이션만 사용해도 되는데, 추가한 제외는 예시로 남겨둔 AppConfig.java 등 다른 설정을 스캔하지 않기 위해 추가작성한 부분임.(실제로는 이부분 불필요)
+public class AutoAppConfig {
+}
