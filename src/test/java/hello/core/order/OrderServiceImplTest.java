6a13904a6b4a3e409c574bc5eb8af8ade51c1f06
INYONG KIM <ohnyong87@gmail.com>
2024-04-09 15:09:54 +0900

    test : 생성자 주입을 사용하는 이유(불변, 누락방지)

--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -1,20 +1,29 @@
 package hello.core.order;
 
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
 
     @Test
     void createOrder() {
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        orderService.createOrder(1L, "itemA", 10000);
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
-    // 만약 수정자 주입으로 OrderServiceImpl 이 주입되는 상황이면 이와 같이 NullPointerException이 발생한다.
-    // createOrder는         Member member = memberRepository.findById(memberId);
-    //        int discountPrice = discountPolicy.discount(member, itemPrice);
-    // 처럼 repository와 discount 의존관계가 들어가있는 로직이기 때문 이 부분이 없다고 나타나는 오류이다.
+    // 생성자 주입으로는 OrderServiceImpl을 생성할때부터 필요한 파라미터를 명시해야 하도록한다.(필수)
+    // 생성자 주입 방식만이 필드에 초기화에 final(불변)을 넣을 수 있기 때문에 좋은 설계가 가능
 
 }
