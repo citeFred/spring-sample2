df52c8ea2c53368d77d2ba612e55295ff962e2ac
INYONG KIM <ohnyong87@gmail.com>
2024-04-09 14:07:34 +0900

    test : 수정자 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private  MemberRepository memberRepository;
+    private  DiscountPolicy discountPolicy;
 
-    //@Autowired //생성자가 1개일땐 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
+    //수정자 주입(setter)
+    @Autowired(required = false)
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
+
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //@Autowired //생성자가 1개일땐 생략 가능
+    //public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //    this.memberRepository = memberRepository;
+    //    this.discountPolicy = discountPolicy;
+    //}
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
